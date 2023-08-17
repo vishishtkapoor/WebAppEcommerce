4d2df33fb58d6c54fef932142374e745f545ea6f
Vishisht Kapoor <138151981+vishishtkapoor@users.noreply.github.com>
2023-08-18 00:26:46 +0530

    Add files via upload

---
+++ src/components/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="./assets/main.png.jpg"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">Ecommerce Website</h5>
+              <p className="card-text fs-5 d-none d-sm-block ">
+                Shop 
+                
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
